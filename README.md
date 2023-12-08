a8f1329fef882d72023f479737724a4210ea09ff
Daniel Beck <beckdp01@gmail.com>
2023-12-08 11:17:28 -0500

    moved personal notes to a notes file ignored by git

--- README.md
+++ README.md
@@ -3,14 +3,3 @@ An updatable compendium of useful open-source intelligence sources worldwide
 
 // in progress!
 
-11/26 Next Task:
-    Implement Edit Buttons (only show them if a user has additional privilege - maybe this is something I leave as a function done by the admin of the database)
-    Add option to add sub jurisdictions to jurisdiction entries
-    Add view of sub jurisdictions on the jurisdiction detail pages
-    Walk through Project: Members Only and add assignment features to this site [then mark as done]
-    Deploy
-    Improve Styling on Sign Up Errors
-
-
-dbeck2
-Kentucky1
--- controllers/jurisdictionController.js
+++ controllers/jurisdictionController.js
@@ -18,7 +18,9 @@ exports.jurisdiction_list = asyncHandler(async (req, res, next) => {
 // Display detail page for a specific Jurisdiction.
 exports.jurisdiction_detail = asyncHandler(async (req, res, next) => {
     const [jurisdiction, allSourcesInJurisdiction] = await Promise.all([
-      Jurisdiction.findById(req.params.id).exec(),
+      Jurisdiction.findById(req.params.id)
+        .populate("subjurisdictions")
+        .exec(),
       Source.find({ jurisdiction: req.params.id }, "name description").exec(),
     ]);
 
@@ -31,35 +33,59 @@ exports.jurisdiction_detail = asyncHandler(async (req, res, next) => {
     res.render("jurisdiction_detail", {
       title: "Jurisdiction Detail",
       jurisdiction: jurisdiction,
+      subjurisdictions: jurisdiction.subjurisdictions,
       jurisdiction_sources: allSourcesInJurisdiction,
       user: req.user
     });
 });
 
 // Display jurisdiction create form on GET.
-exports.jurisdiction_create_get = (req, res, next) => {
-    res.render("jurisdiction_form", {title: "Create Jurisdiction", user: req.user });
-};
+exports.jurisdiction_create_get = asyncHandler(async (req, res, next) => {
+  const allJurisdictions = await Jurisdiction.find().sort({name:1}).exec();
+  res.render("jurisdiction_form", {
+    title: "Create Jurisdiction",
+    subjurisdictions: allJurisdictions, 
+    user: req.user });
+  });
 
 // Handle jurisdiction create on POST.
 exports.jurisdiction_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.subjurisdictions)) {
+      req.body.subjurisdictions =
+        typeof req.body.subjurisdictions === "undefined" ? [] : [req.body.subjurisdictions];
+    }
+    next();
+  },
+
   body("name")
     .trim()
     .isLength({ min: 3 })
     .escape()
     .withMessage("Name must be at least three characters."),
+  body("subjurisdictions.*").escape(),
   
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
 
     const jurisdiction = new Jurisdiction({
       name: req.body.name,
+      subjurisdictions: req.body.subjurisdictions,
     });
 
     if (!errors.isEmpty()) {
+      const allJurisdictions = await Jurisdiction.find().sort({name:1}).exec();
+
+      for (const subjurisdiction of allJurisdictions) {
+        if (jurisdiction.subjurisdictions.includes(subjurisdiction._id)) {
+          subjurisdiction.checked = "true";
+        }
+      }
+
       res.render("jurisdiction_form", {
         title: "Create Jurisdiction",
         jurisdiction: jurisdiction,
+        subjurisdictions: allJurisdictions, 
         user: req.user,
         errors: errors.array(),
       });
